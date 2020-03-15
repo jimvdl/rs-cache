5ddffc602d88e31c12246b0369538d289e74b15f
jimvdl <jimvdlind@gmail.com>
2020-03-15 09:58:00 +0100

    expanded examples, more work todo

--- src/cache.rs
+++ src/cache.rs
@@ -170,9 +170,11 @@ fn index_version(buffer: &[u8]) -> io::Result<u32> {
 mod djd2 {
     pub fn hash(string: &str) -> i32 {
         let mut hash = 0;
+
         for index in 0..string.len() {
             hash = string.chars().nth(index).unwrap() as i32 + ((hash << 5) - hash);
         }
+        
         hash
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -54,11 +54,12 @@ fn encode_checksum() {
 #[test]
 fn validate_checksum() {
     let cache = setup();
+    let checksum = cache.create_checksum().unwrap();
 
     let crcs = vec![1593884597, 1029608590, 16840364, 4209099954, 3716821437, 165713182, 686540367, 
                     4262755489, 2208636505, 3047082366, 586413816, 2890424900, 3411535427, 3178880569, 
                     153718440, 3849392898, 0, 2813112885, 1461700456, 2751169400, 2927815226];
-    let valid = cache.create_checksum().unwrap().validate_crcs(&crcs);
+    let valid = checksum.validate_crcs(&crcs);
 
     assert_eq!(true, valid);
 }
