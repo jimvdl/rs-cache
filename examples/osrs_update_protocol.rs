0fde3d275ca3dfd02345b124857c755db70ccb80
jimvdl <jimvdlind@gmail.com>
2021-08-19 22:12:17 +0200

    improved readability of examples a bit

--- examples/osrs_update_protocol.rs
+++ examples/osrs_update_protocol.rs
@@ -1,37 +1,36 @@
 use rscache::{ Cache, store::MemoryStore };
 
+struct IncomingUpdatePacket {
+    pub index_id: u8,
+    pub archive_id: u32,
+}
+
 // This example illustrates the osrs update protocol.
 // You can use this to handle client requests for cache data.
 fn main() -> rscache::Result<()> {
-    let cache: Cache<MemoryStore> = Cache::new("./data/osrs_cache")?;
+	let cache: Cache<MemoryStore> = Cache::new("./data/osrs_cache")?;
+	let packet = IncomingUpdatePacket{ index_id: 255, archive_id: 10 };
 
-    let index_id = 255;
-    let archive_id = 10;
-
-    let mut buffer = if index_id == 255 && archive_id == 255 {
+    let mut buffer = if packet.index_id == 255 && packet.archive_id == 255 {
 		cache.create_checksum()?.encode_osrs()?
 	} else {
-		let mut buf = cache.read(index_id, archive_id)?;
-		if index_id != 255 {
-			buf.truncate(buf.len() - 2);
-			buf
-		} else {
-			buf
-		}
+		let buf = cache.read(packet.index_id, packet.archive_id)?;
+        format_buffer(buf, packet.index_id)
 	};
 
 	let compression = buffer[0];
-	let length = u32::from_be_bytes([buffer[1], buffer[2], buffer[3], buffer[4]]);
+	let length = parse_length(&buffer);
 
 	buffer.drain(..5);
 
 	let mut data = vec![0; buffer.len() + 8];
-	data[0] = index_id;
-	data[1..3].copy_from_slice(&(archive_id as u16).to_be_bytes());
+	data[0] = packet.index_id;
+	data[1..3].copy_from_slice(&(packet.archive_id as u16).to_be_bytes());
 	data[3] = compression;
 	data[4..8].copy_from_slice(&length.to_be_bytes());
 	data[8..].copy_from_slice(&buffer);
 
+	// Adds separators (255) to tell the client to keep reading bytes.
 	let chunks = data.len() / 512;
 	for index_id in (0..data.len() + chunks).step_by(512) {
 		if index_id == 0 || data.len() == 512 {
@@ -47,4 +46,17 @@ fn main() -> rscache::Result<()> {
     println!("{:?}", data);
     
 	Ok(())
+}
+
+fn format_buffer(mut buffer: Vec<u8>, index_id: u8) -> Vec<u8> {
+    if index_id != 255 {
+        buffer.truncate(buffer.len() - 2);
+        buffer
+    } else {
+        buffer
+    }
+}
+
+fn parse_length(buffer: &[u8]) -> u32 {
+	u32::from_be_bytes([buffer[1], buffer[2], buffer[3], buffer[4]])
 }
--- examples/rs3_update_protocol.rs
+++ examples/rs3_update_protocol.rs
@@ -3,44 +3,35 @@ use rscache::{ Cache, store::FileStore };
 pub const EXPONENT: &'static [u8] = b"5206580307236375668350588432916871591810765290737810323990754121164270399789630501436083337726278206128394461017374810549461689174118305784406140446740993";
 pub const MODULUS: &'static [u8] = b"6950273013450460376345707589939362735767433035117300645755821424559380572176824658371246045200577956729474374073582306250298535718024104420271215590565201";
 
+struct IncomingUpdatePacket {
+    pub index_id: u8,
+    pub archive_id: u32,
+    pub packet_id: u8
+}
+
 // This example illustrates the rs3 update protocol.
 // You can use this to handle client requests for cache data.
 fn main() -> rscache::Result<()> {
     let cache: Cache<FileStore> = Cache::new("./data/rs3_cache")?;
+    let packet = IncomingUpdatePacket{ index_id: 255, archive_id: 10, packet_id: 0 };
 
-    let index_id = 255;
-    let archive_id = 10;
-    let packet_id = 0;
-
-    let buf = if index_id == 255 && archive_id == 255 {
+    let buf = if packet.index_id == 255 && packet.archive_id == 255 {
         cache.create_checksum()?.encode_rs3(EXPONENT, MODULUS)?
     } else {
-        let mut buf = cache.read(index_id, archive_id)?;
-
-        if index_id != 255 {
-            buf.truncate(buf.len() - 2);
-            buf
-        } else {
-            buf
-        }
+        let buf = cache.read(packet.index_id, packet.archive_id)?;
+        format_buffer(buf, packet.index_id)
     };
 
-    // TODO: improve readability
     for data_block in buf.chunks(102395) {
-        let mut data = if index_id == 255 && archive_id == 255 {
-            vec![0; data_block.len() + 10]
-        } else {
-            vec![0; data_block.len() + 5]
-        };
+        let mut data = allocate_buffer(packet.index_id, packet.archive_id, data_block.len());
 
-        data[0] = index_id;
-        data[1..=4].copy_from_slice(&(if packet_id == 0 { archive_id | !0x7fffffff } else { archive_id }).to_be_bytes());
-        
-        if index_id == 255 && archive_id == 255 {
-            data[6..=9].copy_from_slice(&(buf.len() as u32).to_be_bytes());
-            data[10..].copy_from_slice(&buf);
+        encode_index_id(&mut data, packet.index_id);
+        encode_archive_id(&mut data, packet.archive_id, packet.packet_id);
+        if packet.index_id == 255 && packet.archive_id == 255 {
+            encode_length(&mut data, buf.len() as u32);
+            encode_remaining(&mut data[10..], &buf);
         } else {
-            data[5..].copy_from_slice(data_block);
+            encode_remaining(&mut data[5..], &buf);
         }
         
         // write data to the client
@@ -50,4 +41,44 @@ fn main() -> rscache::Result<()> {
     }
  
 	Ok(())
+}
+
+fn format_buffer(mut buffer: Vec<u8>, index_id: u8) -> Vec<u8> {
+    if index_id != 255 {
+        buffer.truncate(buffer.len() - 2);
+        buffer
+    } else {
+        buffer
+    }
+}
+
+fn allocate_buffer(index_id: u8, archive_id: u32, len: usize) -> Vec<u8> {
+    if index_id == 255 && archive_id == 255 {
+        vec![0; len + 10]
+    } else {
+        vec![0; len + 5]
+    }
+}
+
+fn encode_index_id(buffer: &mut [u8], index_id: u8) {
+    buffer[0] = index_id;
+}
+
+fn encode_archive_id(buffer: &mut [u8], archive_id: u32, packet_id: u8) {
+    // packet_id 1 means it is a priority packet, 0 means no priority.
+    let archive_id = if packet_id == 0 { 
+        archive_id | !0x7fffffff 
+    } else { 
+        archive_id 
+    };
+
+    buffer[1..=4].copy_from_slice(&archive_id.to_be_bytes());
+}
+
+fn encode_length(buffer: &mut [u8], length: u32) {
+    buffer[6..=9].copy_from_slice(&length.to_be_bytes());
+}
+
+fn encode_remaining(buffer: &mut [u8], buf: &[u8]) {
+    buffer.copy_from_slice(buf);
 }
