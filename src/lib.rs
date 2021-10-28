5b3cae4fff2313f6438cbdcf9d70b43bbc4c2be1
jimvdl <jimvdlind@gmail.com>
2021-10-28 16:24:09 +0200

    Fixed a grammar mistake

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! A immutable, high-level API for the RuneScape cache file system.
+//! An immutable, high-level API for the RuneScape cache file system.
 //! 
 //! This crate provides convenient access to the binary file system of the [Oldschool RuneScape] and [RuneScape 3] caches.
 //!
