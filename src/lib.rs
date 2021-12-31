b8e5a4436209fdc7f1c368d7af7bd7e3a13a2786
jimvdl <jimvdlind@gmail.com>
2021-12-31 10:56:23 +0100

    Simpler clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -67,40 +67,15 @@
 //! [`Huffman`]: crate::util::Huffman
 //! [`IsaacRand`]: crate::util::IsaacRand
 
-#![deny(clippy::all, clippy::nursery)]
-#![warn(
-    clippy::clone_on_ref_ptr,
-    clippy::redundant_clone,
-    clippy::default_trait_access,
-    clippy::expl_impl_clone_on_copy,
-    clippy::explicit_into_iter_loop,
-    clippy::explicit_iter_loop,
-    clippy::manual_filter_map,
-    clippy::filter_map_next,
-    clippy::manual_find_map,
-    clippy::get_unwrap,
-    clippy::items_after_statements,
-    clippy::large_digit_groups,
-    clippy::map_flatten,
-    clippy::match_same_arms,
-    clippy::maybe_infinite_iter,
-    clippy::mem_forget,
-    clippy::missing_inline_in_public_items,
-    clippy::multiple_inherent_impl,
-    clippy::mut_mut,
-    clippy::needless_continue,
-    clippy::needless_pass_by_value,
-    clippy::map_unwrap_or,
-    clippy::unused_self,
-    clippy::similar_names,
-    clippy::single_match_else,
-    clippy::too_many_lines,
-    clippy::type_repetition_in_bounds,
-    clippy::unseparated_literal_suffix,
-    clippy::used_underscore_binding,
-    clippy::should_implement_trait,
-    clippy::no_effect
+#![deny(
+    clippy::all,
+    clippy::correctness,
+    clippy::suspicious,
+    clippy::style,
+    clippy::complexity,
+    clippy::perf
 )]
+
 #[macro_use]
 pub mod util;
 mod archive;
